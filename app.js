6d880729de423640092c35fa1158fe3743d3b6d6
MohammedAghil <57040494+MohammedAghil@users.noreply.github.com>
2021-11-21 11:29:21 -0500

    Initial structure
    This is just a basic skeleton of the student discussion forum

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const mainRoutes = require('./routes/mainRoutes');
+const host = 'localhost';
+const port = '3000';
+
+app.set('view engine','ejs');
+app.use('/',mainRoutes);
+app.listen(port,host,()=>{
+    console.log('Server is running at '+host+':'+port);
+})
