27ff103ad4f2181e22261e9cf4edb28a4bd410f5
MohammedAghil <57040494+MohammedAghil@users.noreply.github.com>
2021-11-29 18:38:46 -0500

    Added Basic Routing

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-
 const mainRoutes = require('./routes/mainRoutes');
+const postsRoutes = require('./routes/postsRoutes');
+const userRoutes = require('./routes/userRoutes');
 const host = 'localhost';
 const port = '3000';
-
+const db_url = 'mongodb://localhost:27017/StudentDiscussionForum';
 app.set('view engine','ejs');
+app.use(express.static('public'));
+
+mongoose.connect(db_url).then(connected=>{
+    console.log('Succesfully Connected to Database');
+    app.listen(port,host,()=>{
+        console.log('Server is running at '+host+':'+port);
+    });
+}).catch(err=>console.log(err));
 app.use('/',mainRoutes);
-app.listen(port,host,()=>{
-    console.log('Server is running at '+host+':'+port);
-})
+app.use('/posts',postsRoutes);
+app.use('/users',userRoutes);
+
